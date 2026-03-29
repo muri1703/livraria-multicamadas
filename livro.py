736b1be38f9394c6724cddd6b33960f6ac146a85
muri1703 <murilorocharegatieri1703@gmail.com>
2026-03-29 20:43:14 -0300

    Create livro.py

---
+++ livro.py
@@ -0,0 +1,6 @@
+class Livro:
+    def __init__(self, titulo, autor, preco, genero):
+        self.titulo = titulo
+        self.autor = autor
+        self.preco = preco
+        self.genero = genero
